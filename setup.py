902540fd0dd890315ebb3afa1c8b05f13fb5e160
Sidharth S <sidharthsud2701@gmail.com>
2022-01-30 19:47:29 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="metaimg",
+    version="0.0.1",
+    author="Sidharth S",
+    author_email="sidharth.tradis@gmail.com",
+    description="A package to read metadata of images and create clean copies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sidharth-S",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
