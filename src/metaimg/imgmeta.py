6d14954793f259d877440ea81b07e8ecab685e75
Sidharth S <sidharthsud2701@gmail.com>
2022-01-30 21:52:30 +0530

    Stuff (Pathlib,etc)

---
+++ src/metaimg/imgmeta.py
@@ -0,0 +1,53 @@
+from PIL import Image
+from PIL.ExifTags import TAGS
+from pathlib import Path, PurePath
+import os
+
+class metaimg:
+    def __init__(self,image_path) -> None:
+        self.image = image_path
+
+        u_image = Image.open(self.image)
+        exifdata = u_image.getexif()
+
+        self.meta = []
+
+        for tag_id in exifdata:
+            tag = TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            self.meta.append([tag,data])
+        if len(self.meta) == 0:
+            self.meta = None
+    
+    def cleanimage(self,replace = False):
+        im1 = Image.open(self.image)
+        im2 = im1.copy()
+        
+        fp = PurePath(self.image)
+
+        if replace == True :
+            im2.save(str(fp))
+            return
+
+        directory = fp.parent
+        fname = f"Clean - {fp.name}"
+        new_path = str(directory.joinpath(fname))
+        im2.save(new_path)
+        return 
+
+    def metacsv(self):
+        return
+
+
+        
+if __name__ == "__main__":
+    test_images = [
+                    PurePath('./test_images/1.jpg'),
+                    PurePath('./test_images/5.jpg'),
+                  ]
+
+    for i in test_images:
+        x = metaimg(image_path = str(i))
+        print(x.meta)
+        #x.cleanimage(replace=True)
+        print("")
