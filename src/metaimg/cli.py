fa8ce78f01b21ce9b0a421112eb7d742e40e9f46
Sidharth S <sidharthsud2701@gmail.com>
2022-02-02 22:43:26 +0530

    stuff :)

--- src/metaimg/cli.py
+++ src/metaimg/cli.py
@@ -1,12 +1,17 @@
 import argparse
-
-
-import argparse
-from msilib.schema import Error
-from parso import parse
+from pathlib import PurePath
 import tabulate
 
 class cli:
+    def tabulate(self,data):
+        return
+    
+    def createcsv(self):
+        return
+
+    def cleancopy(self):
+        return
+
     def __init__(self) -> None:
         parser = argparse.ArgumentParser(
             description = "CLI application for viewing metadata of images and saving them, and creating clean copies."
@@ -31,14 +36,29 @@ class cli:
                             help = "Save image metadata as a csv file "
                            )
         
-
-
-        
+        parser.add_argument(
+                            "--view","-v",
+                            action="store_true",
+                            help = "View metadata of image(s) [table]"
+                           )
 
         args = parser.parse_args()
 
         if args.file and args.directory:
             raise ValueError("Error: Use only 1 command => --file / --directory")
+
+        image_list = []
+
+        if args.file:
+            suffix = str(PurePath(args.file[0]).suffix)
+            if suffix not in [".jpg",".jpeg",".png",".raw",".dng"]:
+                raise ValueError("Error: Invalid filetype")
+            
+            image_list.append(args.file[0])
+
+        print(image_list)
+    
+
 
 if __name__ == "__main__":
     x = cli()
